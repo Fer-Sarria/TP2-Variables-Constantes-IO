e8cee2668dc7fee007c04547c07a63e6bb02cf54
Fer-Sarria <fer.cabj.007@gmail.com>
2026-07-28 01:36:04 -0300

    Resolución Trabajo Práctico N° 2 - Unidad 4

--- TP2-Variables-Constantes-IO/Program.cs
+++ TP2-Variables-Constantes-IO/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace TP2_Variables_Constantes_IO
+﻿using System;
+namespace TP2_Variables_Constantes_IO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ejercicio1.Ejecutar();
+        }
+    }
+    public class Ejercicio1
+    {
+        public static void Ejecutar()
+        {
+            Console.Clear();
+            Console.Write("Por favor, proporciona tu Nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ahora, podrias decirme tu apellido: ");
+            string apellido = Console.ReadLine();
+
+            Console.Write("¿Que edad tienes?: ");
+            string edad = Console.ReadLine();
+
+            Console.WriteLine("Que bueno que tengas {0} años, {1} {2}!", edad, nombre, apellido);
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
         }
     }
 }
