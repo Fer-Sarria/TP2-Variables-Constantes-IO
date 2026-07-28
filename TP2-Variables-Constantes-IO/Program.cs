9f7d06def517b076379af734101a9b9e280e7245
Fer-Sarria <fer.cabj.007@gmail.com>
2026-07-28 15:59:29 -0300

    Resolución Ejercicio 2

--- TP2-Variables-Constantes-IO/Program.cs
+++ TP2-Variables-Constantes-IO/Program.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿// 1.Registrar datos personales: Solicitar al usuario ingresar su nombre, apellido y edad.
+// Luego muestra un mensaje como: "Hola Juan Pérez, tienes 25 años."
+using System;
 namespace TP2_Variables_Constantes_IO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Ejercicio1.Ejecutar();
+            //Ejercicio1.Ejecutar();
+            Ejercicio2.Ejecutar();
         }
     }
     public class Ejercicio1
@@ -26,5 +29,29 @@ namespace TP2_Variables_Constantes_IO
             Console.WriteLine("Presiona cualquier tecla para salir...");
             Console.ReadKey();
         }
+    }
+}
+
+
+// 2. Calcular el área de un rectángulo: Solicita al usuario ingresar la base y la altura de un
+// rectángulo.Muestra los valores ingresados como: "El valor de la base es: 24", "El valor
+// de la altura es: 45"
+
+public class Ejercicio2 
+{
+    public static void Ejecutar()
+    {
+        Console.Clear();
+        Console.Write("Ingrese la base del rectangulo: ");
+        string baseRect = Console.ReadLine();
+
+        Console.Write("Ingrese la altura del rectangulo: ");
+        string alturaRect = Console.ReadLine();
+
+        Console.WriteLine("El valor de la base es: {0}", baseRect);
+        Console.WriteLine("El valor de la altura es: {0}", alturaRect);
+
+        Console.WriteLine("\nPresiona cualquier tecla para salir...");
+        Console.ReadKey();
     }
 }
