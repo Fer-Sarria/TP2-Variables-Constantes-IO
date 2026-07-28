b9c2ce6836d443cd7655adea2ea261c637f1e34a
Fer-Sarria <fer.cabj.007@gmail.com>
2026-07-27 21:59:10 -0300

    Agregar archivos de proyecto.

---
+++ TP2-Variables-Constantes-IO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP2_Variables_Constantes_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
